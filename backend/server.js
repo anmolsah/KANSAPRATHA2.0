26d4a647eb55c1c48d3dd10775169824c3c1d642
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-29 21:32:54 +0530

    chore: add dotenv configuration and set server port in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+
+//const port = process.env.PORT || 9999;
+const port = 9999;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
