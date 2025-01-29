bf93abdac40340edf7f4c6b9aedcc46bc868d514
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-29 21:35:17 +0530

    testing the backedn server with a message

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const app = express();
 
 //const port = process.env.PORT || 9999;
 const port = 9999;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
