11f29d3a9309d8b5c2610319131fdab4892cf579
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-03 21:02:06 +0530

    feat: simplify admin login async thunk by removing unnecessary response handling

--- dashboard/src/store/reducers/authReducer.js
+++ dashboard/src/store/reducers/authReducer.js
@@ -2,18 +2,15 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/api";
 export const admin_login = createAsyncThunk(
   "auth/admin_login",
-  async (info, { rejectWithValue, fulfillWithValue }) => {
+  async (info) => {
     console.log(info);
     try {
       const { data } = await api.post("/admin-login", info, {
         withCredentials: true,
       });
-      localStorage.setItem("accessToken", data.token);
-      //console.log(data);
-      return fulfillWithValue(data);
+      console.log(data);
     } catch (error) {
-      return rejectWithValue(error.response.data);
-      //console.log(error.response.data);
+      console.log(error.response.data);
     }
   }
 );
