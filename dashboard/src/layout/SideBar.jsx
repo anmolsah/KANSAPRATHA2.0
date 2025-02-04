33b0f66079890c57a56e2fc03a7b2268acff1320
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-05 02:27:52 +0530

    feat: update SideBar component with dynamic navigation and additional routes for admin dashboard

--- dashboard/src/layout/SideBar.jsx
+++ dashboard/src/layout/SideBar.jsx
@@ -1,48 +1,38 @@
-// import React from "react";
-
-// const SideBar = () => {
-//   return (
-//     <div>
-//       <div></div>
-//       <div className={`w-[260px] fixed z-50 top-0 h-screen shadow-[0_0_10px_0_rgba(0,0,0,0.1)] transition-all`}>
-//         <div>
-
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SideBar;
-
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { getNav } from "../navigation";
+import { FiLogOut } from "react-icons/fi";
 
 const SideBar = () => {
+  const { pathname } = useLocation();
   const [allNav, setAllNav] = useState([]);
   useEffect(() => {
     const navs = getNav("admin");
     setAllNav(navs);
   }, []);
   return (
-    <div className="w-[260px] fixed top-0 left-0 h-screen bg-white shadow-md flex flex-col p-4">
+    <div className="w-[260px] fixed top-0 left-0 h-screen bg-gray-100 shadow-md flex flex-col p-4">
       <div className="flex items-center justify-center py-4">
         <Link to="/">
           <img
             src="/logo01.png"
             alt="Admin Logo"
-            className="w-48 h-auto object-contain"
+            className="w-52 h-auto object-contain"
           />
         </Link>
       </div>
+
       <nav className="flex flex-col gap-4 mt-4">
         <ul className="space-y-2">
           {allNav.map((n, i) => (
             <li key={i}>
               <Link
                 to={n.path}
-                className="flex items-center gap-3 p-2 text-gray-700 hover:bg-gray-100 rounded-lg transition"
+                className={`flex items-center gap-3 p-2 rounded-lg transition ${
+                  pathname === n.path
+                    ? "bg-blue-200 text-blue-900 font-semibold"
+                    : "text-gray-700 hover:bg-gray-200"
+                }`}
               >
                 <span className="text-xl">{n.icon}</span>
                 <span className="text-lg">{n.title}</span>
@@ -51,6 +41,13 @@ const SideBar = () => {
           ))}
         </ul>
       </nav>
+
+      <button className="text-gray-700 mt-auto  hover:bg-gray-200 flex items-center gap-3 p-2 rounded-lg transition">
+        <span className="text-xl">
+          <FiLogOut />
+        </span>
+        <span className="text-lg">Logout</span>
+      </button>
     </div>
   );
 };
--- dashboard/src/navigation/allNav.js
+++ dashboard/src/navigation/allNav.js
@@ -1,4 +1,14 @@
-import { FiHome } from "react-icons/fi";
+import {
+  FiDollarSign,
+  FiGrid,
+  FiHome,
+  FiLogOut,
+  FiMessageCircle,
+  FiShoppingCart,
+  FiUser,
+  FiUserPlus,
+  FiUserX,
+} from "react-icons/fi";
 
 export const allNav = [
   {
@@ -8,5 +18,53 @@ export const allNav = [
     role: "admin",
     path: "/admin/dashboard",
   },
-  {},
+  {
+    id: 2,
+    title: "Orders",
+    icon: <FiShoppingCart />,
+    role: "admin",
+    path: "/admin/dashboard/orders",
+  },
+  {
+    id: 3,
+    title: "Category",
+    icon: <FiGrid />,
+    role: "admin",
+    path: "/admin/dashboard/category",
+  },
+  {
+    id: 4,
+    title: "Sellers",
+    icon: <FiUser />,
+    role: "admin",
+    path: "/admin/dashboard/sellers",
+  },
+  {
+    id: 5,
+    title: "Payment Request",
+    icon: <FiDollarSign />,
+    role: "admin",
+    path: "/admin/dashboard/payment-request",
+  },
+  {
+    id: 6,
+    title: "Deactive Seller",
+    icon: <FiUserX />,
+    role: "admin",
+    path: "/admin/dashboard/deactive-seller",
+  },
+  {
+    id: 7,
+    title: "Seller Request",
+    icon: <FiUserPlus />,
+    role: "admin",
+    path: "/admin/dashboard/seller-request",
+  },
+  {
+    id: 8,
+    title: "Live Chat",
+    icon: <FiMessageCircle />,
+    role: "admin",
+    path: "/admin/dashboard/live-chat",
+  },
 ];
