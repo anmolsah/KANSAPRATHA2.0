425e5f753fb23a72556d73a1761361dde3b69570
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-04 19:52:58 +0530

    feat: implement authentication middleware to verify access token from cookies

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports.authMiddleware = async (req, res, next) => {
+  const { accessToken } = req.cookies;
+  if (!accessToken) {
+    return res.status(409).json({ message: "Unauthorized" });
+  } else {
+    try {
+      const decodeToken = await jwt.verify(accessToken, process.env.JWT_SECRET);
+      req.role = decodeToken.role;
+      req.id = decodeToken.id;
+      next();
+    } catch (error) {}
+  }
+};
