72aa4d45a505d7f3e3f2d15753205c2815350ff2
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-05 01:54:11 +0530

    feat: refactor SideBar component to use dynamic navigation from getNav function

--- dashboard/src/navigation/allNav.js
+++ dashboard/src/navigation/allNav.js
@@ -1,9 +1,12 @@
 import { FiHome } from "react-icons/fi";
 
 export const allNav = [
-    {
-        id:1,
-        name:'Dashboard',
-        icon:<FiHome />,
-    }
-]
+  {
+    id: 1,
+    title: "Dashboard",
+    icon: <FiHome />,
+    role: "admin",
+    path: "/admin/dashboard",
+  },
+  {},
+];
