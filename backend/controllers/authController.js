5a4aadb8e467b49f09a18c7c0c926e4278551840
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-04 20:09:55 +0530

    feat: add user retrieval functionality for admin role and implement home route for sellers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -36,33 +36,20 @@ class AuthControllers {
     }
   };
 
-  // admin_login = async (req, res) => {
-  //   const { email, password } = req.body;
+  getUser = async (req, res) => {
+    const { id, role } = req;
 
-  //   try {
-  //     const admin = await adminModel.findOne({ email }).select("+password");
-  //     //console.log(admin);
-  //     if (admin) {
-  //       const match = await bcrypt.compare(password, admin.password);
-  //       if (match) {
-  //         const token = await createToken({
-  //           id: admin.id,
-  //           role: admin.role,
-  //         });
-  //         res.cookie("accessToken", token, {
-  //           expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
-  //         });
-  //         responseReturn(res, 200, { token, message: "login successfull" });
-  //       } else {
-  //         responseReturn(res, 404, { error: "password is incorrect" });
-  //       }
-  //     } else {
-  //       responseReturn(res, 404, { error: "email not found" });
-  //     }
-  //   } catch (error) {
-  //     responseReturn(res, 500, { error: error.message });
-  //   }
-  // };
+    try {
+      if (role === "admin") {
+        const user = await adminModel.findById(id);
+        responseReturn(res, 200, { userInfo: user });
+      } else {
+        console.log("seller info");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 }
 
 module.exports = new AuthControllers();
