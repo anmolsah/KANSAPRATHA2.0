b79e628ce4013dba838a544e91528a1ad49dbf8e
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-26 21:18:55 +0530

    feat: add image upload functionality to AddProduct component

--- dashboard/src/views/seller/AddProduct.jsx
+++ dashboard/src/views/seller/AddProduct.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { BsImage } from "react-icons/bs";
 
 const AddProduct = () => {
   const categorys = [
@@ -84,6 +85,8 @@ const AddProduct = () => {
   const [category, setCategory] = useState("");
   const [allCategory, setAllCategory] = useState(categorys);
   const [searchValue, setSearchValue] = useState("");
+  const [images, setImages] = useState([]);
+  const [imageShow, setImageShow] = useState([]);
 
   const categorySearch = (e) => {
     const value = e.target.value;
@@ -97,6 +100,32 @@ const AddProduct = () => {
       setAllCategory(categorys);
     }
   };
+
+  const imageHandle = (e) => {
+    const files = e.target.files;
+    const length = files.length;
+    if (length > 0) {
+      setImages([...images, ...files]);
+      let imageUrl = [];
+      for (let i = 0; i < length; i++) {
+        imageUrl.push({ url: URL.createObjectURL(files[i]) });
+      }
+      setImageShow([...imageShow, ...imageUrl]);
+    }
+  };
+
+  const changeImage = (img,index)=>{
+    if(img){
+      let tempUrl = imageShow;
+      let tempImages = images;
+
+      tempImages[index] = img;
+      tempUrl[index] = {url:URL.createObjectURL(img)};
+      setImageShow([...tempUrl]);
+      setImages([...tempImages]);
+      
+    }
+  }
   return (
     <div className="px-4 md:px-8 py-8 bg-gray-100 min-h-screen lg:ml-[235px] transition-all">
       <div className="w-full p-4 ng-[add some color] rounded-md">
@@ -219,7 +248,7 @@ const AddProduct = () => {
                 />
               </div>
             </div>
-            <div className="flex flex-col w-full gap-1">
+            <div className="flex flex-col w-full gap-1 mb-5">
               <label htmlFor="description" className="">
                 Description
               </label>
@@ -233,6 +262,32 @@ const AddProduct = () => {
                 cols="10"
                 rows="4"
               ></textarea>
+            </div>
+            <div className="grid lg:grid-cols-4 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 gap-3 w-full text-[adds oem color] mb-4">
+              {imageShow.map((img, i) => (
+                <div className="h-[180px] relative">
+                  <label htmlFor={i}>
+                    <img className="w-full h-full rounded-sm" src={img.url} alt="" />
+                  </label>
+                  <input onChange={(e)=>changeImage(e.target.files[0],i)} type="file" id={i} className="hidden" />
+                </div>
+              ))}
+              <label
+                htmlFor="image"
+                className="flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed border-[add some color] w-full text-[adds ome color]"
+              >
+                <span>
+                  <BsImage />
+                </span>
+                <span>Select Image</span>
+              </label>
+              <input
+                onChange={imageHandle}
+                type="file"
+                id="image"
+                multiple
+                className="hidden"
+              />
             </div>
           </form>
         </div>
