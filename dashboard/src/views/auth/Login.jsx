92f49e0ca80490250241893ec36c75f77624d611
Anmol Sah <kumarxxx242424@gmail.com>
2025-03-09 23:23:46 +0530

    feat: enhance login component with loading state and dynamic button text

--- dashboard/src/views/auth/Login.jsx
+++ dashboard/src/views/auth/Login.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { FaGoogle, FaFacebook } from "react-icons/fa";
+import { ClipLoader } from "react-spinners";
+import toast from "react-hot-toast";
 
 const Login = () => {
+
+    const dispatch = useDispatch();
+    const { loader, errorMessage, successMessage } = useSelector(
+      (state) => state.auth
+    );
   const [state, setState] = useState({
     email: "",
     password: "",
@@ -85,11 +93,18 @@ const Login = () => {
             </Link>
           </div>
 
-          <button
+          {/* <button
             type="submit"
             className="w-full px-4 py-2 text-white bg-amber-700 rounded-md shadow-md hover:bg-amber-800 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
           >
             Log In
+          </button> */}
+          <button
+            disabled={loader ? true : false}
+            type="submit"
+            className="w-full px-4 py-2 text-white bg-amber-700 rounded-md shadow-md hover:bg-amber-800 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2"
+          >
+            {loader ? <ClipLoader color="#ffffff" /> : "Sign In"}
           </button>
         </form>
 
