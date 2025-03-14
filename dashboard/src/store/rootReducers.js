02e495c3c35d4747fca30ae04568fba36dc9ea6c
Anmol Sah <kumarxxx242424@gmail.com>
2025-03-14 18:45:18 +0530

    refactor: fix formatting in rootReducer for consistency

--- dashboard/src/store/rootReducers.js
+++ dashboard/src/store/rootReducers.js
@@ -1,10 +1,9 @@
 import authReducer from "./reducers/authReducer";
 import categoryReducer from "./reducers/categoryReducer";
 
-
 const rootReducer = {
   auth: authReducer,
-  category:categoryReducer
+  category: categoryReducer,
 };
 
 export default rootReducer;
