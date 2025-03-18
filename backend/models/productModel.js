e0f1bc98618c039ef8581f303e091e1aeaca655a
Anmol Sah <kumarxxx242424@gmail.com>
2025-03-18 20:36:12 +0530

    feat: add product model with schema definition and text indexing for search optimization

---
+++ backend/models/productModel.js
@@ -0,0 +1,74 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema(
+  {
+    sellerId: {
+      type: Schema.ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    shopName: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: Array,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+productSchema.index(
+  {
+    name: "text",
+    category: "text",
+    brand: "text",
+    description: "text",
+  },
+  {
+    weights: {
+      name: 5,
+      category: 3,
+      brand: 2,
+      description: 1,
+    },
+  }
+);
+
+module.exports = model("products", productSchema);
