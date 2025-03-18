27a74685bf700e0dac9c55e1a88ddf692c8818ea
Anmol Sah <kumarxxx242424@gmail.com>
2025-03-18 21:21:41 +0530

    feat: enhance add_product method to upload images to Cloudinary and save product details

--- backend/controllers/dashboard/productController.js
+++ backend/controllers/dashboard/productController.js
@@ -1,13 +1,57 @@
 const formidable = require("formidable");
 const { responseReturn } = require("../../utilities/response");
 const cloudinary = require("cloudinary").v2;
+const productModel = require("../../models/productModel");
 
 class productController {
   add_product = async (req, res) => {
     const form = formidable({ multiples: true });
     form.parse(req, async (err, field, files) => {
-      console.log(files);
-      console.log(field);
+      let {
+        name,
+        category,
+        description,
+        stock,
+        price,
+        discount,
+        shopName,
+        brand,
+      } = field;
+
+      const { images } = files;
+      name = name.trim();
+      const slug = name.split(" ").join("-");
+
+      cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        secure: true,
+      });
+
+      try {
+        let allImageUrl = [];
+        for (let i = 0; i < images.length; i++) {
+          const result = await cloudinary.uploader.upload(images[i].filepath, {
+            folder: "products",
+          });
+          allImageUrl = [...allImageUrl.result.url];
+        }
+
+        await productModel.create({
+          name,
+          slug,
+          category,
+          description,
+          stock,
+          price,
+          discount,
+          shopName,
+          brand,
+          images: allImageUrl,
+          
+        });
+      } catch (error) {}
     });
   };
 }
