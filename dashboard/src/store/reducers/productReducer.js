ca9dbba800d5e1b22632aa553e23a75a202ba574
Anmol Sah <kumarxxx242424@gmail.com>
2025-03-21 22:10:37 +0530

    feat: add success handling for product image update in productReducer

--- dashboard/src/store/reducers/productReducer.js
+++ dashboard/src/store/reducers/productReducer.js
@@ -150,6 +150,10 @@ export const productReducer = createSlice({
         state.loader = false;
         state.product = payload.product;
         state.successMessage = payload.message;
+      })
+      .addCase(product_image_update.fulfilled, (state, { payload }) => {
+        state.product = payload.product;
+        state.successMessage = payload.message;
       });
   },
 });
