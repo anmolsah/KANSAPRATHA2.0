d5422c877eacd3aeadb829b7c069b9929296896b
Anmol Sah <kumarxxx242424@gmail.com>
2025-03-23 19:57:34 +0530

    feat: update profile information handling in authController and authReducer

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -155,20 +155,20 @@ class AuthControllers {
   };
 
   profile_info_add = async (req, res) => {
-    const { division, district, shopName, sub_district } = req;
+    const { division, district, shopName, sub_district } = req.body;
     const { id } = req;
 
     try {
       await sellerModel.findByIdAndUpdate(id, {
         shopInfo: {
+          shopName,
           division,
           district,
-          shopName,
           sub_district,
         },
       });
 
-      const userInfo = await sellerModal.findById(id);
+      const userInfo = await sellerModel.findById(id);
       responseReturn(res, 201, {
         message: "Profile info updated successfully",
         userInfo,
