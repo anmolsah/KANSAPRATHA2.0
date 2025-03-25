360e6cac700ba7a9e1c4d64563a46d5f6e3728ee
Anmol Sah <kumarxxx242424@gmail.com>
2025-03-25 20:57:35 +0530

    feat: create Home page and Header component; set up routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-600 font-bold underline'>Hello World</div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full bg-[add]'>
+        <div className='header-top bg-[add] '>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
