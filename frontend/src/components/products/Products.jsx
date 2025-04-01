97d397ff108210b1a5751ae8fe7bb0fc6ee79f22
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-01 23:36:07 +0530

    feat: add responsive breakpoints for carousel in Products component

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -7,6 +7,25 @@ const Products = () => {
     [1, 2, 3],
     [4, 5, 6],
   ];
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <div className="flex gap-8">
       <Carousel
