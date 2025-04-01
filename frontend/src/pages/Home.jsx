fae8bab2afd8e1c7fe3ed3eb6902f6d038936dfe
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-01 23:25:17 +0530

    feat: add Products component and integrate it into Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Categorys from "../components/Categorys";
 import FeatureProduct from "../components/products/FeatureProduct";
+import Products from "../components/products/Products";
 
 const Home = () => {
   return (
@@ -12,6 +13,15 @@ const Home = () => {
       <Categorys />
       <div className="py-[45px]">
         <FeatureProduct />
+      </div>
+      <div className="py-10">
+        <div className="w-[85%] mx-auto flex flex-wrap">
+          <div className="grid w-full grid-col-3  gap-7">
+            <div>
+              <Products />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
