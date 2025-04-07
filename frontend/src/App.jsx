22a3bd381425e1177fe6edbcb09c0566f4242d1d
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-07 19:23:17 +0530

    feat: add Shops page and integrate routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Shops from './pages/Shops';
 
 const App = () => {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/shops" element={<Shops />} />
     </Routes>
    </BrowserRouter>
   )
