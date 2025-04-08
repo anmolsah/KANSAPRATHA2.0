eafff968f821698b440f4ea517b619795eefdad9
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-08 21:34:11 +0530

    feat: add price range filter to Shops page

--- frontend/src/pages/Shops.jsx
+++ frontend/src/pages/Shops.jsx
@@ -4,9 +4,13 @@ import Footer from "./../components/Footer";
 import img1 from "../assets/img1.jpg";
 import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
+import { Range } from "react-range";
 
 const Shops = () => {
   const [filter, setFilter] = useState(true);
+  const [state, setState] = useState({
+    values: [50, 5000],
+  });
   const categorys = [
     "Cookware & Kitchenware",
     "Drinkware",
@@ -82,6 +86,38 @@ const Shops = () => {
                   </div>
                 ))}
               </div>
+
+              <div className="py-2 flex flex-col gap-5">
+                <h2 className="text-3xl font-bold mb-3 text-slate-600">
+                  Price
+                </h2>
+
+                <Range
+                  step={5}
+                  min={50}
+                  max={5000}
+                  values={state.values}
+                  onChange={(values) => setState({ values })}
+                  renderTrack={({ props, children }) => (
+                    <div
+                      {...props}
+                      className="w-full h-[6px] bg-slate-200 rounded-full cursor-pointer"
+                    >
+                      {children}
+                    </div>
+                  )}
+                  renderThumb={({ props }) => (
+                    <div
+                      {...props}
+                      className="w-[15px] h-[15px] bg-indigo-500 rounded-full cursor-pointer"
+                    />
+                  )}
+                />
+              </div>
+
+              <span className="text-slate-800 font-bold text-lg">
+                ${Math.floor(state.values[0])} - ${Math.floor(state.values[1])}
+              </span>
             </div>
           </div>
         </div>
