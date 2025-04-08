f16100ede554bf923823601806c0f42305302853
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-08 20:59:45 +0530

    feat: add category filter checkboxes to Shops page

--- frontend/src/pages/Shops.jsx
+++ frontend/src/pages/Shops.jsx
@@ -7,6 +7,17 @@ import { IoIosArrowForward } from "react-icons/io";
 
 const Shops = () => {
   const [filter, setFilter] = useState(true);
+  const categorys = [
+    "Cookware & Kitchenware",
+    "Drinkware",
+    "Tableware & Dinnerware",
+    "Home Decor",
+    "Gifting",
+    "Brass Products",
+    "Copper Products",
+    "Best Sellers",
+    "New Arrivals",
+  ];
   return (
     <div>
       <Header />
@@ -52,7 +63,25 @@ const Shops = () => {
                   : "md:h-auto md:overflow-auto md:mb-0"
               }`}
             >
-              <h2>Category</h2>
+              <h2 className="text-3xl font-bold mb-3 text-slate-600">
+                Category
+              </h2>
+              <div className="py-2">
+                {categorys.map((c, i) => (
+                  <div
+                    className="flex justify-start items-center gap-2 py-2"
+                    key={i}
+                  >
+                    <input type="checkbox" id={c} />
+                    <label
+                      htmlFor={c}
+                      className="text-slate-600 block cursor-pointer"
+                    >
+                      {c}
+                    </label>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
