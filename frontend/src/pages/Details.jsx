5bd502ee1ec4ff7df072f9ceee29b9a89370c3ff
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-21 12:24:37 +0530

    feat: enhance product display in Details page with discount and ratings

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -387,8 +387,31 @@ const Details = () => {
                     <Link className="block">
                       <div className="relative h-[270px]">
                         <div className="w-full h-full">
-                          <img src={`/images/products/${p}.jpg`} alt="" />
-                          <div className="absolute h-full w-full top-0 bg-black opacity-25 hover:opacity-50 transition-all duration-500"></div>
+                          <img
+                            className="h-full w-full"
+                            src={`/images/products/${p}.jpg`}
+                            alt=""
+                          />
+                          <div className="absolute h-full w-full top-0 left-0 bg-black opacity-25 hover:opacity-50 transition-all duration-500"></div>
+                        </div>
+                        {discount !== 0 && (
+                          <div className="flex justify-center items-center absolute text-white w-[30px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2">
+                            {discount}%
+                          </div>
+                        )}
+                      </div>
+
+                      <div className="p-4 flex flex-col gap-1">
+                        <h2 className="text-slate-600 text-lg font-bold">
+                          Product Name
+                        </h2>
+                        <div className="flex justify-start items-center gap-3">
+                          <h2 className="text-slate-600 text-lg font-bold">
+                            ₹2563
+                          </h2>
+                          <div className="flex items-center gap-2">
+                            <Ratings ratings={4} />
+                          </div>
                         </div>
                       </div>
                     </Link>
@@ -396,6 +419,10 @@ const Details = () => {
                 );
               })}
             </Swiper>
+          </div>
+
+          <div className="w-full flex justify-center items-center py-10">
+            <div className="custom_bullet justify-center gap-3 !w-auto"></div>
           </div>
         </div>
       </section>
