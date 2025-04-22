0a53d40e77c54573888b8e58a2def39b069cc035
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-22 23:15:22 +0530

    feat: implement category fetching in Home component and add API setup

--- frontend/src/store/reducers/homeReducer.js
+++ frontend/src/store/reducers/homeReducer.js
@@ -1,4 +1,19 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const get_category = createAsyncThunk(
+  "product/get_category",
+  async (_, { fulfillWithValue }) => {
+    try {
+      const { data } = await api.get("/home/get-categorys");
+      console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      console.log(error.response);
+      //return fulfillWithValue(error.response.data);
+    }
+  }
+);
 
 export const homeReducer = createSlice({
   name: "home",
