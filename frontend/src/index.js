6f43c73e9906c042a9b0c10005b5a0ea4b5a365c
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-22 21:18:59 +0530

    feat: set up Redux store and integrate with main application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/index";
+import { Toaster } from "react-hot-toast";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Suspense>
+      <App />
+      <Toaster
+        toastOptions={{
+          position: "top-center",
+          style: {
+            background: "#283046",
+            color: "#ffffff",
+          },
+        }}
+      />
+    </Suspense>
+  </Provider>
 );
-
-
-
