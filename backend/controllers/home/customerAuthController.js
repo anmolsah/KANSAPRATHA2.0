c8c3e14cce4ea0d4eaab4e774a24d9c33e75a87d
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-26 18:14:53 +0530

    refactor: clean up customer registration logic and improve token handling

--- backend/controllers/home/customerAuthController.js
+++ backend/controllers/home/customerAuthController.js
@@ -1,36 +1,46 @@
 const customerModel = require("../../models/customerModel");
 const { responseReturn } = require("../../utilities/response");
 const bcrypt = require("bcrypt");
-sellerCustomerModel = require("../../models/chat/sellerCustomerModel");
-
+const sellerCustomerModel = require("../../models/chat/sellerCustomerModel");
+const { createToken } = require("../../utilities/tokenCreate");
 
 class customerAuthController {
-    customer_register = async (req, res) => {
-        const { name, email, password } = req.body;
-        try {
-           const customer = await customerModel.findOne({ email });
-            if (customer) {
-                responseReturn(res, 400, { error: "Customer already exists" });
-            }else{
-                const createCustomer = await customerModel.create({
-                    name:name.trim(),
-                    email:email.trim(),
-                    password:await bcrypt.hash(password, 10),
-                    method:'menualy'
-                })
-                await sellerCustomerModel.create({
-                    myId:createCustomer.id
-                })
-            }
-           
+  customer_register = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+      const customer = await customerModel.findOne({ email });
+      if (customer) {
+        responseReturn(res, 400, { error: "Customer already exists" });
+      } else {
+        const createCustomer = await customerModel.create({
+          name: name.trim(),
+          email: email.trim(),
+          password: await bcrypt.hash(password, 10),
+          method: "menualy",
+        });
+        await sellerCustomerModel.create({
+          myId: createCustomer.id,
+        });
+        const token = await createToken({
+          id: createCustomer.id,
+          name: createCustomer.name,
+          email: createCustomer.email,
+          method: createCustomer.method,
+        });
 
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: "Internal server error" });
-            
-        }
+        res.cookie("customerToken", token, {
+          expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
+        });
+        responseReturn(res, 200, {
+          message: "Customer registered successfully",
+          token,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
     }
+  };
 }
-
 
 module.exports = new customerAuthController();
