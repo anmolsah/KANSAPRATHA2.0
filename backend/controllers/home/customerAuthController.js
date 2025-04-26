a61d57260c8bc7962cd47c7d0dbc24613df88534
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-26 18:14:35 +0530

    feat: implement customer registration logic and create customer model

--- backend/controllers/home/customerAuthController.js
+++ backend/controllers/home/customerAuthController.js
@@ -1,9 +1,29 @@
+const customerModel = require("../../models/customerModel");
+const { responseReturn } = require("../../utilities/response");
+const bcrypt = require("bcrypt");
+sellerCustomerModel = require("../../models/chat/sellerCustomerModel");
 
 
 class customerAuthController {
     customer_register = async (req, res) => {
+        const { name, email, password } = req.body;
         try {
-            console.log(req.body)
+           const customer = await customerModel.findOne({ email });
+            if (customer) {
+                responseReturn(res, 400, { error: "Customer already exists" });
+            }else{
+                const createCustomer = await customerModel.create({
+                    name:name.trim(),
+                    email:email.trim(),
+                    password:await bcrypt.hash(password, 10),
+                    method:'menualy'
+                })
+                await sellerCustomerModel.create({
+                    myId:createCustomer.id
+                })
+            }
+           
+
         } catch (error) {
             console.log(error);
             res.status(500).json({ message: "Internal server error" });
