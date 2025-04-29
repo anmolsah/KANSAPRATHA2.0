bd7e2aa364d8e6744840780adad15642853d3cf2
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-29 20:26:50 +0530

    feat: refactor order placement logic to improve data handling and error management

--- backend/controllers/order/orderController.js
+++ backend/controllers/order/orderController.js
@@ -1,21 +1,73 @@
 const moment = require("moment");
 const customerOrder = require("../../models/customerOrder");
-const authorOrder = require("../../models/authOrder");
+const authorOrderModel = require("../../models/authOrder");
+const cartModel = require("../../models/cartModel");
+const { responseReturn } = require("../../utilities/response");
 
 class orderController {
   place_order = async (req, res) => {
-    const {
-      price,
-      products,
-      shipping_fee,
-      shippingInfo,
-      userId,
-    } = req.body;
+    const { price, products, shipping_fee, shippingInfo, userId } = req.body;
     let authorOrderData = [];
     let cartId = [];
     const tempDate = moment(Date.now()).format("LLL");
+    let customerOrderProduct = [];
+    for (let i = 0; i < products.length; i++) {
+      const pro = products[i].products;
+
+      for (let j = 0; j < pro.length; j++) {
+        const tempCusPro = pro[j].productInfo;
+        tempCusPro.quantity = pro[j].quantity;
+        customerOrderProduct.push(tempCusPro);
+        if (pro[j]._id) {
+          cartId.push(pro[j]._id);
+        }
+      }
+    }
+
     try {
-    } catch (error) {}
+      const order = await customerOrder.create({
+        customerId: userId,
+        shippingInfo,
+        products: customerOrderProduct,
+        price: price + shipping_fee,
+        payment_status: "pending",
+        delivery_status: "unpaid",
+        date: tempDate,
+      });
+      for (let i = 0; i < products.length; i++) {
+        const pro = products[i].products;
+        const pri = products[i].price;
+        const sellerId = products[i].sellerId;
+        let storePro = [];
+        for (let j = 0; j < pro.length; j++) {
+          const tempPro = pro[j].productInfo;
+          tempPro.quantity = pro[j].quantity;
+          storePro.push(tempPro);
+        }
+        authorOrderData.push({
+          orderId: order.id,
+          sellerId,
+          products: storePro,
+          price: pri,
+          payment_status: "unpaid",
+          shippingInfo: "KhansaPratha Main Warehouse",
+          delivery_status: "pending",
+          date: tempDate,
+        });
+      }
+
+      await authorOrderModel.insertMany(authorOrderData);
+      for (let k = 0; k < cartId.length; k++) {
+        await cartModel.findByIdAndDelete(cartId[k]);
+      }
+      responseReturn(res, 201, {
+        message: "Order placed successfully",
+        orderId: order.id,
+      });
+    } catch (error) {
+        console.log(error);
+        responseReturn(res, 500, { error: "Internal server error" });
+    }
   };
 }
 
