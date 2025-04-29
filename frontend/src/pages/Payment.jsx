55cd7e56640fe106db2a25d28378673619df3cdb
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-29 21:11:17 +0530

    Add stripe.webp image to the public directory

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React,{useState} from "react";
+import Header from "./../components/Header";
+import Footer from "./../components/Footer";
+import { useLocation } from "react-router-dom";
 
 const Payment = () => {
+  const {
+    state: { price, items, orderId },
+  } = useLocation();
+  const [paymentMethod, setPaymentMethod] = React.useState("stripe");
   return (
-    <div>Payment</div>
-  )
-}
+    <div>
+      <Header />
 
-export default Payment
+      <section className="bg-[#eeeeee]">
+        <div className="w-[85%] mx-auto py-16 mt-4">
+          <div className="flex flex-wrap">
+            <div className="w-7/12">
+              <div className="pr-2">
+                <div className="flex flex-wrap">
+                  <div>
+                    <div className="flex flex-col gap-[3px] justify-center items-center">
+                      <img src="/stripe.webp" alt="" />
+                    </div>
+                    <span className="text-slate-600">Stripe</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default Payment;
