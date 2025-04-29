4ed29943d7bbd48bd75d42bc3cc52b789342e5fe
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-29 19:45:12 +0530

    feat: implement order and customer order models with schema definitions

--- backend/controllers/order/orderController.js
+++ backend/controllers/order/orderController.js
@@ -1,6 +1,19 @@
+const moment = require("moment");
+const customerOrder = require("../../models/customerOrder");
+const authorOrder = require("../../models/authOrder");
+
 class orderController {
   place_order = async (req, res) => {
-    console.log(req.body);
+    const {
+      price,
+      products,
+      shipping_fee,
+      shippingInfo,
+      userId,
+    } = req.body;
+    let authorOrderData = [];
+    let cartId = [];
+    const tempDate = moment(Date.now()).format("LLL");
     try {
     } catch (error) {}
   };
