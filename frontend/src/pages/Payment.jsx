9706f53846d0498fbb60b64d06e2ea58bba4566c
Anmol Sah <kumarxxx242424@gmail.com>
2025-04-30 12:14:28 +0530

    feat: enhance Payment component with conditional rendering for payment methods

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "./../components/Header";
 import Footer from "./../components/Footer";
 import { useLocation } from "react-router-dom";
-import Stripe from './../components/Stripe';
+import Stripe from "./../components/Stripe";
 
 const Payment = () => {
   const {
@@ -42,19 +42,37 @@ const Payment = () => {
                     <span className="text-slate-600">COD</span>
                   </div>
                 </div>
-              </div>
 
-              {
-                paymentMethod === "stripe" && <div>
-                  <Stripe />
+                {paymentMethod === "stripe" && (
+                  <div>
+                    <Stripe />
+                  </div>
+                )}
+                {paymentMethod === "cod" && (
+                  <div className="w-full px-4 py-8 bg-white rounded-md shadow-md">
+                    <button className="px-10 py-[6px] rounded-sm hover:shadow-green-500/20 hover:shadow-lg bg-green-500 text-white">
+                      Pay Now
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="w-5/12">
+              <div className="pl-2">
+                <div className="bg-white shadow p-5 text-slate-600 flex flex-col gap-3">
+                  <h3 className="font-bold text-lg">Order Summary</h3>
+                  <div className="flex justify-between items-center">
+                    <span>{items} Items and Shipping Fee Included</span>
+                    <span>₹{price}</span>
+                  </div>
+
+                  <div className="flex justify-between items-center font-semibold">
+                    <span>Total Amount</span>
+                    <span>₹{price}</span>
+                  </div>
                 </div>
-                  
-              }
-              {
-                paymentMethod === "cod" && <div className="w-full px-4 py-8 bg-white rounded-md shadow-md">
-                  <button className="px-10 py-[6px] rounded-sm hover:shadow-green-500/20 hover:shadow-lg bg-green-500 text-white">Pay Now</button>
-                </div>
-              }
+              </div>
             </div>
           </div>
         </div>
