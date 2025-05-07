48111541040e23c6b56e67d0e9cffa4eb6567862
Anmol Sah <kumarxxx242424@gmail.com>
2025-05-07 18:57:50 +0530

    feat: implement get_admin_orders functionality; add pagination and search support in admin orders retrieval

--- backend/controllers/order/orderController.js
+++ backend/controllers/order/orderController.js
@@ -171,6 +171,52 @@ class orderController {
       responseReturn(res, 500, { error: "Internal server error" });
     }
   };
+
+  get_admin_orders = async (req, res) => {
+    let { page, searchValue, perPage } = req.query;
+    page = parseInt(page);
+    perPage = parseInt(perPage);
+    const skipPage = perPage * (page - 1);
+
+    try {
+      if (searchValue) {
+      } else {
+        const orders = await customerOrder
+          .aggregate([
+            {
+              $lookup: {
+                from: "authororders",
+                localField: "_id",
+                foreignField: "orderId",
+                as: "suborder",
+              },
+            },
+          ])
+          .skip(skipPage)
+          .limit(perPage)
+          .sort({ createdAt: -1 });
+
+        const totalOrder = await customerOrder.aggregate([
+          {
+            $lookup: {
+              from: "authororders",
+              localField: "_id",
+              foreignField: "orderId",
+              as: "suborder",
+            },
+          },
+        ]);
+
+        responseReturn(res, 200, {
+          orders,
+          totalOrder: totalOrder.length,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      responseReturn(res, 500, { error: "Internal server error" });
+    }
+  };
 }
 
 module.exports = new orderController();
