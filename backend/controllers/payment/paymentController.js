fd94d0803f264e16389ef66a2ddfb4f07d706f1f
Anmol Sah <kumarxxx242424@gmail.com>
2025-05-08 14:13:27 +0530

    feat: implement stripe model and update dependencies; add stripe and uuid package updates

--- backend/controllers/payment/paymentController.js
+++ backend/controllers/payment/paymentController.js
@@ -1,3 +1,8 @@
+const stripeModel = require("../../models/stripeModel");
+const { responseReturn } = require("../../utilities/response");
+const {v4: uuidv4} = require('uuid');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 class paymentController {
   create_stripe_connect_account = async (req, res) => {
     console.log("testing ...");
