b7dfbd3ea9349c363304791b36826b6717e868bb
Anmol Sah <kumarxxx242424@gmail.com>
2025-05-08 18:55:31 +0530

    feat: refactor create_stripe_connect_account method; improve error handling and code structure

--- backend/controllers/payment/paymentController.js
+++ backend/controllers/payment/paymentController.js
@@ -1,14 +1,52 @@
 const stripeModel = require("../../models/stripeModel");
 const { responseReturn } = require("../../utilities/response");
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 class paymentController {
   create_stripe_connect_account = async (req, res) => {
-    console.log("testing ...");
-    console.log(req.id);
+    const { id } = req;
+    const uid = uuidv4();
     try {
-    } catch (error) {}
+      const stripeInfo = await stripeModel.findOne({ sellerId: id });
+      if (stripeInfo) {
+        await stripeModel.deleteOne({ sellerId: id });
+        const account = await stripe.accounts.create({
+          type: "express",
+        });
+        const accountLink = await stripe.accountLinks.create({
+          account: account.id,
+          refresh_url: process.env.STRIPE_REDIRECT_URL,
+          return_url: `http://localhost:9000/success?activeCode=${uid}`,
+          type: "account_onboarding",
+        });
+        await stripeModel.create({
+          sellerId: id,
+          stripeId: account.id,
+          code: uid,
+        });
+        responseReturn(res, 200, { url: accountLink.url });
+      } else {
+        const account = await stripe.accounts.create({
+          type: "express",
+        });
+        const accountLink = await stripe.accountLinks.create({
+          account: account.id,
+          refresh_url: process.env.STRIPE_REDIRECT_URL,
+          return_url: `http://localhost:9000/success?activeCode=${uid}`,
+          type: "account_onboarding",
+        });
+        await stripeModel.create({
+          sellerId: id,
+          stripeId: account.id,
+          code: uid,
+        });
+        responseReturn(res, 200, { url: accountLink.url });
+      }
+    } catch (error) {
+      console.log(error);
+      responseReturn(res, 500, { error: "Internal server error" });
+    }
   };
 }
 
