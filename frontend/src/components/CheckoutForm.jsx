54e56a9d3c4e2aa6fd4b06e5e1c096e64e9962a3
Anmol Sah <kumarxxx242424@gmail.com>
2025-05-16 13:35:09 +0530

    feat: implement CheckoutForm and integrate payment processing with Stripe

--- frontend/src/components/CheckoutForm.jsx
+++ frontend/src/components/CheckoutForm.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  PaymentElement,
+  LinkAuthenticationElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ orderId }) => {
+  localStorage.setItem("orderId", orderId);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
+  const submit = async(e)=>{
+      e.preventDefault();
+  }
+
   return (
-    <div>CheckoutForm</div>
-  )
-}
+    <form onSubmit={submit} id="payment-form" className="">
+      <LinkAuthenticationElement id="link-authentication-element" />
+      <PaymentElement id="payment-element" options={paymentElementOptions} />
+      <button
+        disabled={isLoading || !stripe || !elements}
+        id="submit"
+        className="px-10 py-[6px] rounded-sm hover:shadow-green-500/20 hover:shadow-lg bg-green-500 text-white"
+      >
+        <span id="button-text">
+          {isLoading ? <div>Loading...</div> : "Pay Now"}
+        </span>
+      </button>
+      {
+        /* Show any error or success messages */
+        message && <div>{message}</div>
+      }
+    </form>
+  );
+};
 
-export default CheckoutForm
+export default CheckoutForm;
--- frontend/src/components/Stripe.jsx
+++ frontend/src/components/Stripe.jsx
@@ -18,16 +18,35 @@ const Stripe = ({ orderId, price }) => {
     clientSecret,
   };
 
-
-  return <div className="mt-4">
-    {
-      clientSecret ? (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      ):<button className="px-10 py-[6px] rounded-sm hover:shadow-green-500/20 hover:shadow-lg bg-green-500 text-white">Start Payment</button>
+  const create_payment = async () => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:9999/api/order/create-payment",
+        { price },
+        { withCredentials: true }
+      );
+      setClientSecret(data.clientSecret);
+    } catch (error) {
+      console.log(error);
     }
-  </div>;
+  };
+
+  return (
+    <div className="mt-4">
+      {clientSecret ? (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm orderId={orderId} />
+        </Elements>
+      ) : (
+        <button
+          onClick={create_payment}
+          className="px-10 py-[6px] rounded-sm hover:shadow-green-500/20 hover:shadow-lg bg-green-500 text-white"
+        >
+          Start Payment
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Stripe;
