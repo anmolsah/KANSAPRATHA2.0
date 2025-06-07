20138d1a280c5a3ef8a352d3ab0650773b8c076f
Anmol Sah <kumarxxx242424@gmail.com>
2025-06-08 00:50:17 +0530

    feat: enhance admin dashboard data retrieval with total sales, products, orders, and sellers

--- backend/controllers/dashboard/dashboardController.js
+++ backend/controllers/dashboard/dashboardController.js
@@ -1,11 +1,36 @@
+const { responseReturn } = require("../../utilities/response");
+const myShopWallet = require("../../models/myShopWallet");
+const productModel = require("../../models/productModel");
+const customerOrder = require("../../models/customerOrder");
+const sellerModel = require("../../models/sellerModel");
+
 class dashboardController {
   get_admin_dashboard_data = async (req, res) => {
-    
-
     try {
-        
+      const totalSale = await myShopWallet.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalAmount: { $sum: "$amount" },
+          },
+        },
+      ]);
+
+      const totalProduct = await productModel.find({}).countDocuments();
+      const totalOrder = await customerOrder.find({}).countDocuments();
+      const totalSeller = await sellerModel.find({}).countDocuments();
+      //const messages = await will do it further
+      const recentOrders = await customerOrder.find({}).limit(5);
+      responseReturn(res, 200, {
+        totalProduct,
+        totalOrder,
+        totalSeller,
+        recentOrders,
+        totalSale: totalSale.length > 0 ? totalSale[0].totalAmount : 0,
+      });
     } catch (error) {
-        
+      console.log(error);
+      responseReturn(res, 500, { error: "Internal server error" });
     }
   };
 }
